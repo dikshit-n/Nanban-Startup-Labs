c2caf70223fc4429c149080fff25c815ee9b21f6
Dikshitkumar <62920150+dikshitkumarn@users.noreply.github.com>
2020-06-22 18:42:48 +0530

    Flexible list display.

---
+++ assets/js/dropText.js
@@ -0,0 +1,153 @@
+var drop1 = document.getElementById("d1")
+var listItem1 = document.getElementById("l1")
+var listContainer1 = document.getElementById("lc1")
+
+var drop2 = document.getElementById("d2")
+var listItem2 = document.getElementById("l2")
+var listContainer2 = document.getElementById("lc2")
+
+var drop3 = document.getElementById("d3")
+var listItem3 = document.getElementById("l3")
+var listContainer3 = document.getElementById("lc3")
+
+var drop4 = document.getElementById("d4")
+var listItem4 = document.getElementById("l4")
+var listContainer4 = document.getElementById("lc4")
+
+var drop5 = document.getElementById("d5")
+var listItem5 = document.getElementById("l5")
+var listContainer5 = document.getElementById("lc5")
+
+var drop6 = document.getElementById("d6")
+var listItem6 = document.getElementById("l6")
+var listContainer6 = document.getElementById("lc6")
+
+var drop7 = document.getElementById("d7")
+var listItem7 = document.getElementById("l7")
+var listContainer7 = document.getElementById("lc7")
+
+var drop8 = document.getElementById("d8")
+var listItem8 = document.getElementById("l8")
+var listContainer8 = document.getElementById("lc8")
+
+var drop9 = document.getElementById("d9")
+var listItem9 = document.getElementById("l9")
+var listContainer9 = document.getElementById("lc9")
+
+var drop10 = document.getElementById("d10")
+var listItem10 = document.getElementById("l10")
+var listContainer10 = document.getElementById("lc10")
+
+var drop11 = document.getElementById("d11")
+var listItem11 = document.getElementById("l11")
+var listContainer11 = document.getElementById("lc11")
+
+var drop12 = document.getElementById("d12")
+var listItem12 = document.getElementById("l12")
+var listContainer12 = document.getElementById("lc12")
+
+var drop13 = document.getElementById("d13")
+var listItem13 = document.getElementById("l13")
+var listContainer13 = document.getElementById("lc13")
+
+var drop14 = document.getElementById("d14")
+var listItem14 = document.getElementById("l14")
+var listContainer14 = document.getElementById("lc14")
+
+drop1.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem1.className = this.expand ? "large" : "small"
+    listContainer1.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop2.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem2.className = this.expand ? "large" : "small"
+    listContainer2.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop3.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem3.className = this.expand ? "large" : "small"
+    listContainer3.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop4.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem4.className = this.expand ? "large" : "small"
+    listContainer4.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop5.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem5.className = this.expand ? "large" : "small"
+    listContainer5.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop6.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem6.className = this.expand ? "large" : "small"
+    listContainer6.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop7.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem7.className = this.expand ? "large" : "small"
+    listContainer7.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop8.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem8.className = this.expand ? "large" : "small"
+    listContainer8.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop9.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem9.className = this.expand ? "large" : "small"
+    listContainer9.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop10.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem10.className = this.expand ? "large" : "small"
+    listContainer10.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop11.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem11.className = this.expand ? "large" : "small"
+    listContainer11.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop12.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem12.className = this.expand ? "large" : "small"
+    listContainer12.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop13.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem13.className = this.expand ? "large" : "small"
+    listContainer13.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
+
+drop14.addEventListener("click", function(e) {
+    e.preventDefault()
+    this.expand = !this.expand;
+    listItem14.className = this.expand ? "large" : "small"
+    listContainer14.className = this.expand ? "listItemLarge" : "listItemSmall"
+})
